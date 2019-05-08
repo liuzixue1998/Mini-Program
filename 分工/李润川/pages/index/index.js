e43aba55f1948f0bc5aa4765426312d8bb280711
chuan695309771 <695309771@qq.com>
2019-05-08 17:14:54 +0800

    目的地页面

---
+++ 分工/李润川/pages/index/index.js
@@ -0,0 +1,9 @@
+var getData = require('../../data/data.js');
+var app = getApp();
+Page({
+  data: {
+    signupimg: '../imgs/China.png',
+    imgsrc: '../imgs/China.png',
+    iconsrc: '../imgs/China.png',
+  },
+})
