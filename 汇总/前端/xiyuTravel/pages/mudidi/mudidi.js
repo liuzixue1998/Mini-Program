b6aaa84450ce6acdb7fa47f1c25c32ba60f1f788
yinyujia <1373722811@qq.com>
2019-05-28 14:56:11 +0800

    一些修改

--- 汇总/前端/xiyuTravel/pages/mudidi/mudidi.js
+++ 汇总/前端/xiyuTravel/pages/mudidi/mudidi.js
@@ -4,9 +4,9 @@ var getData = require('../../data/data.js');
 var app = getApp();
 Page({
   data: {
-    signupimg: '../../images/China.png',
-    imgsrc: '../../images/China.png',
-    iconsrc: '../../images/China.png',
+    // signupimg: '../../images/China.png',
+    // imgsrc: '../../images/China.png',
+    // iconsrc: '../../images/China.png',
   },
   change: function () {
     wx.navigateTo({
